7b16a29c459d938313fe1533a37d971cc59f14c9
Amir Blum <amir@aspecto.io>
2021-01-06 14:27:14 +0200

    test ci

--- packages/package1/lib/package1.js
+++ packages/package1/lib/package1.js
@@ -6,4 +6,5 @@ function package1() {
     // updating only package 1 
     // bump patch version
     // change to package 1 in addition to common dep
+    // change to package 1 only - test in CI
 }
